2290ae9d8445faff17bc5f3e245705cceaff870a
nata1901 <natasha_djalikovo@mail.ru>
2023-04-21 01:00:02 +0900

    Переместили Promo в отдельную папку

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
-const Promo = (props) => {
-    // props - это объект, в который можно передать разные свойства для компонента
-    let name = "promo";
-    switch (props.type) {
-        case "lg": name = "promo big"; break;
-        case "sm": name = "promo small"; break;
-        default: name = "promo";
-    }
-   
-    return (
-        <div className={name}>
-            <div className={props.pic ? "promo-pic" : "promo-pic pic2"}></div>
-                <h3>{props.text}</h3>
-        </div>
-    )
+import Promo from "./components/Promo/Promo";
+
+
+const sizes = ["sm", "lg", "md"];
+const adds = [];
+
+let text = "Полёты собак в космос — серия биологических экспериментов, включавших проведение исследований по возможности полётов на геофизических и космических ракетах живых существ, наблюдение за поведением высокоорганизованных животных в условиях таких полётов, а также, изучение сложных явлений в околоземном пространстве."
+text = text.match(/[^\s,.]+/g);
+console.log(text);
+
+const rand = (n) => Math.floor(Math.random() * n);
+
+let n = 8;
+while (n--) {
+    adds.push({
+        text: `${text[rand(text.length)]} ${text[rand(text.length)]} ${text[rand(text.length)]}`,
+        pic: !!Math.round(Math.random()), // !!0 => false - !!1 => true
+        sizes: sizes[rand(sizes.length)]
+    })
 }
 
 const App = () => {
-    return (
-        <div title="Doggy">
-            <h1>Hello!</h1>
-            <hr />
-            <i>i</i>
-            <i />
-            <h2>
-                <mark>React</mark>
-            </h2>
+    return <div>
             <div className="container">
-            <Promo text="My Promo" type="lg"/>
-            <Promo text="^_^"/>
-            <Promo text="Doggy" pic={false}/>
-            <Promo type="sm"/>
-            <Promo text="The Dogs" type="sm"/>
+           {adds.map(el => <Promo {...el} type={el.sizes}/>)}
             </div>
         </div>
-    )
 }
 
 export default App;
